8b0dbff8f708cbacbe0b3a7981b6755a5575ecfa
shinoda <tetora1053@gmail.com>
2019-07-19 21:18:27 +0900

    make generator with closure

--- src/closure.go
+++ src/closure.go
@@ -14,9 +14,23 @@ func getClosure() func(int, string) (int, string) {
 	}
 }
 
+func integers() func() int{
+	var i int
+	return func() int {
+		current := i
+		i++
+		return current
+	}
+}
+
 func main() {
 	f := getClosure()
 	fmt.Println(f(1, "Go"))
 	fmt.Println(f(2, "Hello"))
 	fmt.Println(f(3, "World"))
+
+	ints := integers()
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
 }
