e79701dbd6bc3692f36a6e6708361c71d670f722
shinoda <tetora1053@gmail.com>
2020-05-16 17:10:25 +0900

    バブルソート、選択ソート

---
+++ algorithm/sort/bubble_sort.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bubbleSort(s []int) (sw int) {
+	flag := true
+	i := 0
+	for flag {
+		flag = false
+		for j:=len(s)-1; j>i; j-- {
+			if s[j] < s[j-1] {
+				s[j], s[j-1] = s[j-1], s[j]
+				flag = true
+				sw++
+			}
+		}
+	}
+	return
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	s := make([]int, n)
+	for i:=0; i<n; i++ {
+		fmt.Scan(&s[i])
+	}
+	
+	sw := bubbleSort(s)
+	fmt.Println(s)
+	fmt.Println(sw)
+}
---
+++ algorithm/sort/selection_sort.go
@@ -0,0 +1,31 @@
+// 1行目に数列の長さを表す整数Nを、2行目にN個の整数が空白区切りで与えられる。
+// 選択ソートのアルゴリズムで昇順に並べ換えよ。
+package main
+
+import (
+	"fmt"
+)
+
+func selectionSort(s []int) {
+	for i:=0; i<len(s); i++ {
+		minj := i
+		for j:=i+1; j<len(s); j++ {
+			if s[j] < s[minj] {
+				minj = j
+			}
+		}
+		s[i], s[minj] = s[minj], s[i]
+	}
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	s := make([]int, n)
+	for i:=0; i<n; i++ {
+		fmt.Scan(&s[i])
+	}
+	selectionSort(s)
+	fmt.Println(s)
+}
