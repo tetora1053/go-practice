ef3ba9f2a3716ce30204ba82a1db9906af7ebc5a
shinoda <tetora1053@gmail.com>
2020-05-18 10:39:05 +0900

    データ構造用のディレクトリ作成。スタック

---
+++ algorithm/data_structure/stack.go
@@ -0,0 +1,53 @@
+// 逆ポーランド記法の入力が与えられるので、スタックを使って計算し、計算結果を出力せよ。
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	"strconv"
+)
+
+var (
+	top int
+	stack []int
+)
+
+func init() {
+	stack = make([]int, 100)
+}
+
+func push(x int) {
+	top++
+	stack[top]= x
+}
+
+func pop() int {
+	top--
+	return stack[top+1]
+}
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	str := sc.Text()
+	s := strings.Split(str, " ")
+	for _, v := range s {
+		if v == "+" {
+			b, a := pop(), pop()
+			push(a+b)
+		} else if v == "-" {
+			b, a := pop(), pop()
+			push(a-b)
+		} else if v == "*" {
+			b, a := pop(), pop()
+			push(a*b)
+		} else {
+			x, _ := strconv.Atoi(v)
+			push(x)
+		}
+	}
+	fmt.Println(pop())
+}
+
---
+++ algorithm/sort/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type C struct {
+	suit string
+	val int
+}
+
+func bubble(s []C) {
+	for i:=0; i<len(s); i++ {
+		for j:=len(s)-1; j>i; j-- {
+			if s[j].val < s[j-1].val {
+				s[j], s[j-1] = s[j-1], s[j]
+			}
+		}
+	}
+}
+
+func selection(s []C) {
+	for i:=0; i<len(s); i++ {
+		minj := i
+		for j:=i+1; j<len(s); j++ {
+			if s[j].val < s[minj].val {
+				minj = j
+			}
+		}
+		s[i], s[minj] = s[minj], s[i]
+	}
+}
+
+func isStable(s1, s2 []C) {
+	for i:=0; i<len(s1); i++ {
+		if s1[i].suit != s2[i].suit {
+			fmt.Println("Not Stable")
+			return
+		}
+	}
+	fmt.Println("Stable")
+}
+
+func main() {
+	var (
+		n int
+		str string
+	)
+	fmt.Scan(&n)
+	s1 := make([]C, n)
+	for i:=0; i<n; i++ {
+		fmt.Scan(&str)
+		suit := string(str[0])
+		val, _ := strconv.Atoi(string(str[1]))
+		s1[i].suit = suit
+		s1[i].val = val
+	}
+	s2 := make([]C, n)
+	copy(s2, s1)
+
+	bubble(s1)
+	fmt.Println(s1)
+	fmt.Println("Stable")
+	selection(s2)
+	fmt.Println(s2)
+	isStable(s1, s2)
+}
+
